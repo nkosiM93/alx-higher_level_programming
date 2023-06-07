0ea3759a27b4e227b6c4cf39fa6d45257d611034
CodeAlpha <mathiyathokozanin@gmail.com>
2023-06-06 21:24:45 -0700

    5. 00...99

--- 0x01-python-if_else_loops_functions/5-print_comb2.py
+++ 0x01-python-if_else_loops_functions/5-print_comb2.py
@@ -1,6 +1,7 @@
 #!/usr/bin/python3
 
 for i in range(100):
-    if i < 10:
-        print("0{},", end="")
-    print("{}, ".format(i), end='')
+    if i < 99:
+        print("{}".format("0" if i < 10 else "") + f"{i}" + ",", end=" ")
+    else:
+        print(i)
