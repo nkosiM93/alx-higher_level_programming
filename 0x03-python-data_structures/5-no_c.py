3c6cbce11218103240595b1843d4cb0e13f6bc16
CodeAlpha <mathiyathokozanin@gmail.com>
2023-06-12 19:58:44 -0700

    5. Can you C me now?

--- 0x03-python-data_structures/5-no_c.py
+++ 0x03-python-data_structures/5-no_c.py
@@ -2,6 +2,5 @@
 
 def no_c(my_string):
     if my_string:
-        for i, char in enumerate(my_string):
-            if char == 'C' or char == 'c':
-                my_string[i] = []
+        return [char for char in my_string 
+                if char != 'C' or char != 'c']
