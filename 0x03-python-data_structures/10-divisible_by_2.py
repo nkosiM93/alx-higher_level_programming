d3a254449e4ecd2131f60ffbdf502745d9176503
CodeAlpha <mathiyathokozanin@gmail.com>
2023-06-13 14:18:30 -0700

    10. Only by 2

---
+++ 0x03-python-data_structures/10-divisible_by_2.py
@@ -0,0 +1,15 @@
+#!/usr/bin/python3
+
+def divisible_by_2(my_list=[]):
+    if (len(my_list) == 0):
+        return []
+    
+    new_lst = []
+
+    for i in my_list:
+        if i % 2 == 0:
+            new_lst.append(True)
+        else:
+            new_lst.append(False)
+
+    return new_lst
