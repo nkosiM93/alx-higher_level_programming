60dacc297717abdfbe593108766cfeed909a7d94
CodeAlpha <mathiyathokozanin@gmail.com>
2023-06-15 05:55:07 -0700

    Q6 Fix

--- 0x04-python-more_data_structures/6-print_sorted_dictionary.py
+++ 0x04-python-more_data_structures/6-print_sorted_dictionary.py
@@ -3,7 +3,8 @@
 def print_sorted_dictionary(a_dictionary):
     if a_dictionary:
         sorted_list = {k: a_dictionary[k] for k in sorted(a_dictionary)}
-    
-    for i, j in sorted_list.items():
-        print(str(i) + ": " + str(j))
 
+        for i, j in sorted_list.items():
+            print(str(i) + ": " + str(j))
+    else:
+        print()
