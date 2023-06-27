1736269540cd56c67fe9851cc65ef9021b2edb0f
CodeAlpha <mathiyathokozanin@gmail.com>
2023-06-27 08:12:51 -0700

    3. Integers division with debug

--- 0x05-python-exceptions/3-safe_print_division.py
+++ 0x05-python-exceptions/3-safe_print_division.py
@@ -8,5 +8,5 @@ def safe_print_division(a, b):
     except (ZeroDivisionError):
         pass
     finally:
-        print("Inside Result: {}".format(answer))
+        print("Inside result: {}".format(answer))
         return answer
