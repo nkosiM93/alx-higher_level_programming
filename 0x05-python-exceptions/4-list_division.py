e3877e813cc477f014103545838fc14bf5945139
CodeAlpha <mathiyathokozanin@gmail.com>
2023-06-27 10:39:32 -0700

    4. Divide a list

--- 0x05-python-exceptions/4-list_division.py
+++ 0x05-python-exceptions/4-list_division.py
@@ -3,7 +3,7 @@
 def list_division(my_list_1, my_list_2, list_length):
 
     answers = []
-    try: 
+    try:
         for i in range(list_length):
             try:
                 answers.append(my_list_1[i] / my_list_2[i])
