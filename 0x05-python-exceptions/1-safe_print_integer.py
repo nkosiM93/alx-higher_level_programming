f5453a820d1d9bcf10dada668dbe1745f8c4891f
CodeAlpha <mathiyathokozanin@gmail.com>
2023-06-26 20:13:05 -0700

    1. Safe printing of an integers list

---
+++ 0x05-python-exceptions/1-safe_print_integer.py
@@ -0,0 +1,13 @@
+#!/usr/bin/python3
+
+def safe_print_integer(value):
+
+    result = False
+
+    try:
+        print("{:d}".format(value))
+        result = True
+    except ValueError:
+        pass
+    return result
+
