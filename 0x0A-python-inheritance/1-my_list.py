963b5e16258111b9da177d49d37eca2ba39aabe6
CodeAlpha <mathiyathokozanin@gmail.com>
2023-07-11 06:29:50 -0700

    Q1 Solution

---
+++ 0x0A-python-inheritance/1-my_list.py
@@ -0,0 +1,10 @@
+#!/usr/bin/python3
+"""Defines a class that inherits from list"""
+
+
+class MyList(list):
+    """defines a list of integers"""
+
+    def print_sorted(self):
+        """Prints the list but sorted"""
+        print(sorted(self))
