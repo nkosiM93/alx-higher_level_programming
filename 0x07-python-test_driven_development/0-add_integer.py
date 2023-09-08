cdfa44d3a60c05af4c496ebb45d9f66d93513984
AlphaDev <mathiyathokozanin@gmail.com>
2023-09-08 03:16:31 +0000

    0. Integers addition

---
+++ 0x07-python-test_driven_development/0-add_integer.py
@@ -0,0 +1,21 @@
+#!/usr/bin/python3
+""" Module contains a function that adds two numbers """
+
+def add_integer(a, b=98):
+	""" Adds two integers
+	
+	Args:
+	a (int): 1st integer
+	b (int): 2nd integer
+
+	"""
+
+	if not isinstance(a, (float, int)):
+		raise TypeError("a must be an integer")
+	if not isinstance(b, (float, int)):
+		raise TypeError("b must be an integer")
+		
+	a = int(a)
+	b = int(b)
+	
+	return a + b
