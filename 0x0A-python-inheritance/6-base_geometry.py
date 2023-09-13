8403ef8ef996df9e968d3a1283acaff9b0c13352
AlphaCoder <mathiyathokozanin@gmail.com>
2023-09-13 00:11:24 -0700

    6. Improve Geometry

---
+++ 0x0A-python-inheritance/6-base_geometry.py
@@ -0,0 +1,9 @@
+#!/usr/bin/python3
+"""Module contains a geometry class"""
+
+
+class BaseGeometry:
+    """Class containing methods and functions that do some geometry"""
+
+    def area(self):
+        raise Exception("area() is not implemented")
