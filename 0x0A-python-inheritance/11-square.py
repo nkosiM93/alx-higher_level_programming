e31c278e157e21ea161d327798f78ec80c521656
AlphaCoder <mathiyathokozanin@gmail.com>
2023-09-13 05:57:55 -0700

    11. Square #2

---
+++ 0x0A-python-inheritance/11-square.py
@@ -0,0 +1,20 @@
+#!/usr/bin/python3
+"""Module contains a class that calculates the area of a sqaure"""
+Rectangle = __import__("9-rectangle").Rectangle
+
+
+class Square(Rectangle):
+    """Class has methods that calculate the area of a square"""
+
+    def __init__(self, size):
+        # super().__init__(size, size)
+        super().integer_validator("size", size)
+        self.__size = size
+
+    def area(self):
+        """returns the the area of a square"""
+
+        return self.__size ** 2i
+
+    def __str__(self):
+        return f"[{self.__class__.__name__}] {self.__size}/{self.__size}"
