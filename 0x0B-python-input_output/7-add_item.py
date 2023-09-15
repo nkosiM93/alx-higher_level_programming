17b482174dbb8ca929ce9a525f606e105dabd8b9
AlphaCoder <mathiyathokozanin@gmail.com>
2023-09-14 20:10:42 -0700

    7. Load, add, save

---
+++ 0x0B-python-input_output/7-add_item.py
@@ -0,0 +1,18 @@
+#!/usr/bin/python3
+"""Load, add, save to a JSON File"""
+import sys
+import json
+load_from_json_file = __import__("6-load_from_json_file").load_from_json_file
+save_to_json_file = __import__("5-save_to_json_file").save_to_json_file
+
+
+myL = load_from_json_file("add_item.json")
+def addToList():
+    if len(sys.argv) > 1:
+        for i in range(len(sys.argv)):
+            if i != 0:
+                myL.append(sys.argv[i])
+
+if __name__ == "__main__":
+    addToList()
+    save_to_json_file(myL, "add_item.json")
