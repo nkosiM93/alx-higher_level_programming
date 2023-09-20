28420ff47d4ac02d4888d7e4031f5da98c513f5c
AlphaCoder <mathiyathokozanin@gmail.com>
2023-09-20 01:28:46 -0700

    6. __str__

--- 0x0C-python-almost_a_circle/models/rectangle.py
+++ 0x0C-python-almost_a_circle/models/rectangle.py
@@ -100,3 +100,7 @@ class Rectangle(Base):
             for j in range(self.__width):
                 print("#", end="")
             print()
+
+    def __str__(self):
+        return (f"[{self.__class__.__name__}] ({self.id}) {self.__x}/{self.__y}"
+                f" - {self.__width}/{self.__height}")
