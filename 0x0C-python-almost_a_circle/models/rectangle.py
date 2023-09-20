1c031122045d9f306a053472ee4c110a23776ed8
AlphaCoder <mathiyathokozanin@gmail.com>
2023-09-19 21:24:58 -0700

    Pycodestyle corrections

--- 0x0C-python-almost_a_circle/models/rectangle.py
+++ 0x0C-python-almost_a_circle/models/rectangle.py
@@ -17,47 +17,39 @@ class Rectangle(Base):
     @property
     def width(self):
         """Gets width"""
-        
         return self.__width
 
     @width.setter
     def width(self, w):
         """Sets width"""
-        
         self.__width = w
 
     @property
     def height(self):
         """Gets width"""
-        
         return self.__height
 
     @height.setter
     def height(self, h):
         """Sets width"""
-        
         self.__height = h
 
     @property
     def x(self):
         """ Sets width"""
-        
         return self.__x
 
     @x.setter
     def x(self, x):
         """ Sets width"""
-        
         self.__x = x
 
     @property
     def y(self):
         """ Sets width"""
-
         return self.__y
 
     @y.setter
     def y(self, y):
         """ Sets width"""
-
         self.__y = y
