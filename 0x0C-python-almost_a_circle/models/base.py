ffa014fbee07b9ab8268ebdf3e636c2d281e4b42
AlphaCoder <mathiyathokozanin@gmail.com>
2023-09-28 05:56:43 -0700

    15. Dictionary to JSON string

--- 0x0C-python-almost_a_circle/models/base.py
+++ 0x0C-python-almost_a_circle/models/base.py
@@ -20,6 +20,6 @@ class Base:
         """Converts object to JSON format"""
 
         if list_dictionaries is None or len(list_dictionaries) == 0:
-            return []
+            return "[]"
         else:
             return json.dumps(list_dictionaries)
