d70ac3101347ba06c1118db61cae5ae7d49a571a
AlphaCoder <mathiyathokozanin@gmail.com>
2023-10-19 22:09:29 -0700

    19. Temperatures #1: Correction

--- 0x0D-SQL_introduction/102-top_city.sql
+++ 0x0D-SQL_introduction/102-top_city.sql
@@ -2,6 +2,7 @@
 -- Syntax below selects the top three cities by temperature average
 SELECT city, AVG(value) AS avg_temp 
 FROM temperatures
+WHERE `month` = 7 OR `month` = 8
 GROUP BY city
 ORDER BY avg_temp DESC
 LIMIT 3;
