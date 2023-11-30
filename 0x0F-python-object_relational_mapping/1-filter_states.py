258d5e11042486d3a5713eb59b0889d054a9047e
AlphaCoder <mathiyathokozanin@gmail.com>
2023-11-30 00:41:20 -0800

    1. Filter states

---
+++ 0x0F-python-object_relational_mapping/1-filter_states.py
@@ -0,0 +1,18 @@
+#!/usr/bin/python3
+""" Module uses MySQLdb to connect to a database and retrieve info """
+import MySQLdb
+import sys
+
+
+if __name__ == "__main__":
+    db = MySQLdb.connect(host="localhost", port=3306, user=sys.argv[1],
+                         passwd=sys.argv[2], db=sys.argv[3])
+    cur = db.cursor()
+
+    cur.execute(f"USE {sys.argv[3]}")
+    cur.execute("SELECT * FROM states WHERE LEFT(name, 1) = 'N'"
+                "ORDER BY id ASC")
+    rows = cur.fetchall()
+
+    for row in rows:
+        print(f"{row}")
