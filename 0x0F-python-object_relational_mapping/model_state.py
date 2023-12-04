99c3a2a966615d23edfb2fbe43e0c0c7a252f35e
AlphaCoder <mathiyathokozanin@gmail.com>
2023-12-04 04:23:25 -0800

    6. First state model

--- 0x0F-python-object_relational_mapping/model_state.py
+++ 0x0F-python-object_relational_mapping/model_state.py
@@ -1,5 +1,5 @@
 #!/usr/bin/python3
-"""Moudule conatains a class that represents a user"""
+# Module conatains a class that represents a user
 from sqlalchemy.ext.declarative import declarative_base
 from sqlalchemy import String, Integer, Column
 from sqlalchemy import create_engine
