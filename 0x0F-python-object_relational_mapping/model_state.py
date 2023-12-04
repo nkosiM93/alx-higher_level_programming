7dbc009ae1315805f5f6a0753e2b8cfc7a9a4da1
AlphaCoder <mathiyathokozanin@gmail.com>
2023-12-04 04:11:00 -0800

    6. First state model

---
+++ 0x0F-python-object_relational_mapping/model_state.py
@@ -0,0 +1,22 @@
+#!/usr/bin/python3
+"""Moudule conatains a class that represents a user"""
+from sqlalchemy.ext.declarative import declarative_base
+from sqlalchemy import String, Integer, Column
+from sqlalchemy import create_engine
+import sys
+
+
+engine = create_engine(f"mysql+mysqldb://{sys.argv[1]}:{sys.argv[2]}"
+                       f"@localhost:3306/{sys.argv[3]}",
+                       pool_pre_ping=True)
+
+Base = declarative_base()
+
+
+class State(Base):
+    """Object representation of a User"""
+
+    __tablename__ = "states"
+    id = Column(Integer, primary_key=True, nullable=False,
+                autoincrement=True)
+    name = Column(String(128), nullable=False)
