29f9f07549f47a3392db54eb65e93dbf8f52c073
AlphaCoder <mathiyathokozanin@gmail.com>
2023-12-05 21:49:47 -0800

    8. First state

---
+++ 0x0F-python-object_relational_mapping/8-model_state_fetch_first.py
@@ -0,0 +1,17 @@
+#!/usr/bin/python3
+"""Prints the first State retieved from a a query of states"""
+import sys
+from sqlalchemy import create_engine
+from sqlalchemy.orm import sessionmaker
+from model_state import State, Base
+
+
+if __name__ == "__main__":
+    engine = create_engine(f"mysql+mysqldb://{sys.argv[1]}:{sys.argv[2]}"
+                           f"@localhost:3306/{sys.argv[3]}",
+                           pool_pre_ping=True)
+    Session = sessionmaker(bind=engine)
+    session = Session()
+    state = session.query(State).first()
+
+    print(f"{state.id}: {state.name}")
