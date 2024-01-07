0e83937b31c4c4cea1abf97fbee6dfaa5acc0ca6
Thokozani N. Ndlovu <122861222+nkosiM93@users.noreply.github.com>
2024-01-07 14:09:25 +0200

    Delete 0x12-javascript-warm_up directory

--- 0x12-javascript-warm_up/11-second_biggest.js
+++
@@ -1,9 +0,0 @@
-#!/usr/bin/node
-
-if (process.argv.length < 4) {
-  console.log('0');
-} else {
-  const copyArr = process.argv.slice(2);
-  copyArr.sort((a, b) => a - b);
-  console.log(copyArr[copyArr.length - 2]);
-}
