889166b80c8f13f87dea434a2a18869360689013
AlphaCoder <mathiyathokozanin@gmail.com>
2024-07-01 02:23:56 -0700

    10

--- 0x12-javascript-warm_up/10-factorial.js
+++ 0x12-javascript-warm_up/10-factorial.js
@@ -2,13 +2,15 @@
 
 function factorial (num) {
   if (num === 1 || num === 0) {
-     return 1;
+    return 1;
   }
   return num * factorial(num - 1);
 }
 
-let num = Number(process.argv[2]);
+const num = Number(process.argv[2]);
 
 if (num) {
   console.log(factorial(num));
+} else {
+  console.log(1);
 }
