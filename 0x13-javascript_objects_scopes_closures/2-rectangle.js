09ab3fd357637558523031a58abd28581f1d276d
AlphaCoder <mathiyathokozanin@gmail.com>
2024-07-08 02:55:35 -0700

    0 - 3

---
+++ 0x13-javascript_objects_scopes_closures/2-rectangle.js
@@ -0,0 +1,11 @@
+#!/usr/bin/node
+
+class Rectangle {
+  constructor(h, w) {
+    if (h > 0 && w > 0) {
+      this.width = w;
+      this.height = h;
+    }
+  }
+}
+module.exports = Rectangle;
---
+++ 0x13-javascript_objects_scopes_closures/3-rectangle.js
@@ -0,0 +1,23 @@
+#!/usr/bin/node
+
+class Rectangle {
+  constructor(w, h) {
+    if (h > 0 && w > 0) {
+      this.width = w;
+      this.height = h;
+    }
+  }
+  print () {
+    let rect = '';  
+    if (this.width) {
+      for (let i = this.height; i > 0; i--) {
+        for (let j = this.width; j > 0; j--) {
+	  rect += 'X';
+	}
+	console.log(rect);
+	rect = '';
+      }
+    }
+  }
+}
+module.exports = Rectangle;
