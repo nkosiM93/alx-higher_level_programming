e0f3b1e860149bab29c1ef05ef74d4895a4ed7a6
AlphaCoder <mathiyathokozanin@gmail.com>
2024-07-08 03:05:44 -0700

    1 - 3 fix

--- 0x13-javascript_objects_scopes_closures/2-rectangle.js
+++ 0x13-javascript_objects_scopes_closures/2-rectangle.js
@@ -1,7 +1,7 @@
 #!/usr/bin/node
 
 class Rectangle {
-  constructor(h, w) {
+  constructor (h, w) {
     if (h > 0 && w > 0) {
       this.width = w;
       this.height = h;
--- 0x13-javascript_objects_scopes_closures/3-rectangle.js
+++ 0x13-javascript_objects_scopes_closures/3-rectangle.js
@@ -1,21 +1,22 @@
 #!/usr/bin/node
 
 class Rectangle {
-  constructor(w, h) {
+  constructor (w, h) {
     if (h > 0 && w > 0) {
       this.width = w;
       this.height = h;
     }
   }
+
   print () {
-    let rect = '';  
+    let rect = '';
     if (this.width) {
       for (let i = this.height; i > 0; i--) {
         for (let j = this.width; j > 0; j--) {
-	  rect += 'X';
-	}
-	console.log(rect);
-	rect = '';
+          rect += 'X';
+        }
+        console.log(rect);
+        rect = '';
       }
     }
   }
