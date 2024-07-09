5e30824ed35f67a6ae0be8cc08fb99770ade03b4
AlphaCoder <mathiyathokozanin@gmail.com>
2024-07-09 07:02:19 -0700

    6,8,9

--- 0x13-javascript_objects_scopes_closures/8-esrever.js
+++ 0x13-javascript_objects_scopes_closures/8-esrever.js
@@ -1,4 +1,8 @@
 #!/usr/bin/node
 exports.esrever = function (list) {
-  return list.reverse();
+  let newArr = [];
+  for (let i = 0; i < list.length; i++) {
+    newArr.unshift(list[i]);
+  }
+  return newArr;
 };
